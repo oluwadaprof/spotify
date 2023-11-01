974f2a95ad013597e707aef1930d75d0eb339459
Oluwadaprof <adeekotobiloba8@gmail.com>
2023-11-01 03:30:02 +0100

    Feat: Added player component

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -3,12 +3,16 @@
 import React from 'react'
 import { Song } from '@/types'
 import SongItem from '../../../components/SongItem'
+import useOnPlay from '@/hooks/useOnPlay'
 
 interface PageContentProps {
     songs: Song[]
 }
 
 const PageContent: React.FC<PageContentProps> = ({songs}) => {
+
+const onPlay = useOnPlay(songs)
+
     if(songs.length === 0){
         return (
             <div className='mt-4 text-neutral-400'>
@@ -30,7 +34,7 @@ const PageContent: React.FC<PageContentProps> = ({songs}) => {
     {songs.map((song)=>(
         <SongItem
         key={song.id}
-        onClick={()=>{}}
+        onClick={(id:string)=>onPlay(id)}
         data={song}
         />
     ))}
